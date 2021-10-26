a6c4d9465dcf72d1805e72c58dc45dc67ad85cc2
ChrisGuy <chrismorgan1526@gmail.com>
2021-10-26 16:37:08 +0100

    Add input field to player name

--- src/components/PlayerInfo.js
+++ src/components/PlayerInfo.js
@@ -1,10 +1,24 @@
 import PlayerTurns from "./PlayerTurns"
+import { useState } from "react"
 
-export default function PlayerInfo() {
+export default function PlayerInfo({ playerNumber }) {
+    const [playerName, setPlayerName] = useState("Insert Name");
+    
+
+    const handleNameChange = (e) => {
+        setPlayerName(e.target.value);
+      }
+
+
     return (
         <div className='PlayerInfo'>
             <PlayerTurns />
-            <h2>Player Name</h2>
+            <input 
+                type="text"
+                value={playerName}
+                onChange={handleNameChange}
+                className="PlayerName"
+                />
             <p className="PlayerScore">
                 Score: 0
             </p>
--- src/components/PlayerPanel.js
+++ src/components/PlayerPanel.js
@@ -1,10 +1,12 @@
 import PlayerInfo from "./PlayerInfo"
 import ScoreCard from "./ScoreCard"
 
-export default function PlayerPanel() {
+export default function PlayerPanel({ playerNumber }) {
     return (
         <div className='PlayerPanel'>
-            <PlayerInfo />
+            <PlayerInfo
+                playerNumber = {playerNumber}    
+            />
             <button>Roll Dice</button>
             <ScoreCard />
         </div>
